1b7d66f056f3accb58abc58698d0084b25b8156e
sureshselvadurai <sureshrajaselvadurai@gmail.com>
2024-10-11 01:41:14 -0700

    xgb model builder

--- modules/ml_developer/XGBoostModelBuilder.py
+++ modules/ml_developer/XGBoostModelBuilder.py
@@ -1,9 +1,13 @@
+import os
+
 from matplotlib import pyplot as plt
 from pyspark.ml.evaluation import RegressionEvaluator
 from xgboost.spark import SparkXGBRegressor
 
+
 class XGBoostModelBuilder:
-    def __init__(self, model_data, feature_columns, label_column):
+    def __init__(self, model_data, feature_columns, label_column, model_name="XGB_model"):
+        self.model_name = model_name
         self.xgb_model = None
         self.dataframe = model_data
         self.feature_columns = feature_columns
@@ -30,9 +34,12 @@ class XGBoostModelBuilder:
         self.xgb_model = xgb_regressor.fit(self.train_df)
         return self.xgb_model
 
-    def evaluate_model(self):
+    def evaluate_model(self, type="Test"):
 
-        predictions = self.xgb_model.transform(self.test_df)
+        if type == "Test":
+            predictions = self.xgb_model.transform(self.test_df)
+        else:
+            predictions = self.xgb_model.transform(self.train_df)
 
         evaluator = RegressionEvaluator(
             label_col=self.label_column,
@@ -48,10 +55,27 @@ class XGBoostModelBuilder:
         sorted_importances = sorted(feature_importances.items(), key=lambda x: x[1], reverse=True)
         return sorted_importances
 
-    def plot_tree(self, model, num_trees=0, num_levels=3):
-        booster = model.nativeBooster
+    def plot_tree(self, num_trees=0, num_levels=3):
+        booster = self.xgb_model.nativeBooster
 
         # Plot the tree for the specified number of levels
         plt.figure(figsize=(12, 8))
         self.xgb_model.plot_tree(booster, num_trees=num_trees, rankdir="UT", max_depth=num_levels)
         plt.show()
+
+    def save_model(self, path=None):
+        if self.xgb_model is not None:
+
+            save_path = path if path else os.path.join(os.getcwd(), self.model_name)
+            self.xgb_model.save(save_path)
+            print(f"Model '{self.model_name}' saved to {save_path}")
+        else:
+            print("Model is not trained yet. Please train the model before saving.")
+
+    @classmethod
+    def load_model(cls, spark_context, model_name, path=None):
+        load_path = path if path else os.path.join(os.getcwd(), model_name)
+        model = SparkXGBRegressor.load(load_path)
+        instance = cls(model.dataframe, model.feature_columns, model.label_column, model_name=model_name)
+        instance.xgb_model = model
+        return instance
